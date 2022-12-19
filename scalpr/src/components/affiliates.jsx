5160065dc38d4184d9ec733271d9eb5f68297822
Andrew <zhengandrew35@gmail.com>
2022-12-19 10:28:28 -0500

    skeleton

---
+++ scalpr/src/components/affiliates.jsx
@@ -0,0 +1,13 @@
+export default function Affiliates () {
+
+
+
+    return (
+        <div className="affiliates">
+            <div >
+                <h2>affiliates</h2>
+                
+            </div>
+        </div>
+    )
+}
