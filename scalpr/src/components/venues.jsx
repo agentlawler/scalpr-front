3fdd63c965dce0994fa8dad92ac9ed0bd8ac440a
Matt <mkiska1@gmail.com>
2022-12-19 11:26:50 -0500

    populated venues/shows and venuedetail/showdetail

--- scalpr/src/components/venues.jsx
+++ scalpr/src/components/venues.jsx
@@ -1,13 +1,31 @@
-export default function Venues () {
+import { useState, useEffect } from "react"
+import { Link } from "react-router-dom"
+import axios from "axios"
 
+export default function Venues() {
+  const [venues, setVenues] = useState([])
+  useEffect(() => {
+    const getVenues = async () => {
+      const response = await axios.get("")
+      setVenues(response.data)
+    }
+    getVenues()
+  }, [])
 
-
-    return (
-        <div className="venues">
-            <div >
-                <h2>venues</h2>
-                
-            </div>
+  return venues ? (
+    <div className="container">
+        <div className="Venues">
+            {venues.map((venue) => (
+                <div className="venue" key={venue.id}>
+                    <Link to={`/venues/${venue.id}`}>
+                        <img src={venue.image} alt={venue.name} />
+                        <h3>{venue.name}</h3>
+                    </Link>
+                </div>
+            ))}
         </div>
-    )
+    </div>
+  ) : (
+    <h1> Loading Please Wait ... </h1>
+  )
 }
