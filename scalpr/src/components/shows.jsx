8cff5946e6815259fa0e0b094e355e92b3f194d6
Devon Jones <linesbydevon@gmail.com>
2022-12-20 10:37:41 -0500

    forgot to switch branches, doing a quick push to be safe

--- scalpr/src/components/shows.jsx
+++ scalpr/src/components/shows.jsx
@@ -64,9 +64,13 @@ function sortByPrice(){
 
   return shows ? (
     <div className="container">
+      
+      <div className="sortContainer">
+      <svg xmlns="http://www.w3.org/2000/svg" height="48" width="48"><path d="M6 36v-3h12v3Zm0-10.5v-3h24v3ZM6 15v-3h36v3Z"/></svg>
       <button onClick={sortByTitle}>Title</button>
       <button onClick={sortByDate}>Date</button>
       <button onClick={sortByPrice}>Lowest Price</button>
+      </div>
 
 
         <div className="shows">
